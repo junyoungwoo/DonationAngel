63e7cf4f9e2c7a2ba0952e642ceff03335ad2077
user2 <user2@KH_A>
2018-01-17 17:21:30 +0900

    test

---
+++ test/src/main/mainClass.java
@@ -0,0 +1,14 @@
+package main;
+
+import view.SponsorChildView;
+import view.main_SponsorView;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		
+		new SponsorChildView();
+		
+	}
+
+}
